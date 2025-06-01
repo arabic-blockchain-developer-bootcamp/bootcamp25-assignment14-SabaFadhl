b9dceb1ef22c60865474ba72e714b3f93c45652d
SabaFadhl <sabafadl98@gmail.com>
2025-06-01 11:46:23 +0300

    feat: compelete unit test

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -10,38 +10,56 @@ contract VaultTest is Test {
     address user = makeAddr("user");
 
     function setUp() public {
-        // TODO: instantiate new instance of the Vault contract
+        // Instantiate a new instance of the Vault contract
         vault = new Vault();
 
-        // TODO: Fund the user with some ether using vm.deal
-
+        // Fund the user with some ether using vm.deal
+        vm.deal(user, 10 ether); // Provide user with 10 ether for testing
     }
 
     function testDeposit() public {
-        // TODO: prank user and call deposit with 1 ether
-       
+        // Prank user and call deposit with 1 ether
+        vm.startPrank(user);
+        vault.deposit{value: 1 ether}();
+        vm.stopPrank();
 
+        // Assert the user's balance in the vault is 1 ether
         assertEq(vault.balances(user), 1 ether);
     }
 
     function testWithdraw() public {
-        // TODO: prank user, deposit 2 ether, withdraw 1 ether
+        // Prank user, deposit 2 ether, and withdraw 1 ether
+        vm.startPrank(user);
+        vault.deposit{value: 2 ether}();
+        vault.withdraw(1 ether);
+        vm.stopPrank();
 
-
+        // Assert the user's balance in the vault is 1 ether
         assertEq(vault.balances(user), 1 ether);
     }
 
     function test_RevertWithdrawMoreThanBalance() public {
-        // TODO: prank user, deposit 1 ether
+        // Prank user and deposit 1 ether
+        vm.startPrank(user);
+        vault.deposit{value: 1 ether}();
 
+        // Expect the transaction to revert
         vm.expectRevert();
 
-        // TODO: try to withdraw 2 ether
+        // Try to withdraw 2 ether
+        vault.withdraw(2 ether);
 
+        vm.stopPrank();
     }
 
     function testGetBalance() public {
-        // TODO: prank user, deposit 0.5 ether, check getBalance
-        // TODO: assert returned balance is 0.5 ether
+        // Prank user, deposit 0.5 ether, and check getBalance
+        vm.startPrank(user);
+        vault.deposit{value: 0.5 ether}();
+        uint256 balance = vault.getBalance();
+        vm.stopPrank();
+
+        // Assert the returned balance is 0.5 ether
+        assertEq(balance, 0.5 ether);
     }
 }
